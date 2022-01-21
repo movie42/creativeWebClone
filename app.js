84ecc2a567b99a29024610b29e7b183e83bdd31a
movie42 <44064122+movie42@users.noreply.github.com>
2022-01-21 10:20:36 +0900

    data setting in about page from prismic

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ dotenv.config();
 
 import express from "express";
 import path from "path";
-import prismicHelper from "@prismicio/helpers";
+import * as prismicH from "@prismicio/helpers";
 import { client } from "./config/prismicConfig.js";
 
 const app = express();
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use((req, res, next) => {
-  res.locals.ctx = {
-    prismicHelper,
-  };
+  res.locals.prismicH = prismicH;
   next();
 });
 
@@ -22,9 +21,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", async (req, res) => {
-  const documents = await client.getFirst();
-  console.log(documents);
-  res.render("pages/about", { documents });
+  const { data } = await client.getFirst();
+  console.log(data);
+  res.render("pages/about", { data });
 });
 
 app.get("/detail/:uid", (req, res) => {
